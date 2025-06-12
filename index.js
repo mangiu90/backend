289add601870d59b30ef1b90e80a44274b9d91a6
Manuel Giudici <giudicimanuel@gmail.com>
2025-06-12 01:02:46 -0300

    remove cors

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
-app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
