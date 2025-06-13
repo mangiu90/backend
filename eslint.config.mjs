dfa961eae00cf8a72ad6600b28658ae00b5b76dd
Manuel Giudici <giudicimanuel@gmail.com>
2025-06-12 21:56:42 -0300

    validation_and_es_lint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+
+export default defineConfig([
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
+    },
+  },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css" },
+  {
+    ignores: ['dist/**'],
+  },
+]);
